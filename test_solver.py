a04bdf8a8959157cd0d19dc00a47d8d54e1eed25
danfh00 <danielfhudson@yahoo.com>
2024-07-11 16:48:31 +0100

    Tests added

--- test_solver.py
+++ test_solver.py
@@ -25,3 +25,22 @@ def test_make_todays_grid(mock_date, test_date, expected_grid_fixture, request):
     expected_grid = request.getfixturevalue(expected_grid_fixture)
 
     assert make_todays_grid() == expected_grid
+
+
+@pytest.mark.parametrize("in_grid_fixture, block_fixture, x, y, expected_output", [
+    ("fake_jan_1_grid", "b_shape", 2, 2, True),
+    ("fake_jan_1_grid", "r_shape", 1, 1, True),
+    ("fake_jan_1_grid", "b_shape", 0, 0, False),
+    ("fake_jun_29_grid", "b_shape", 1, 1, True),
+    ("fake_jun_29_grid", "b_shape", 1, 5, False),
+    ("fake_jun_29_grid", "r_shape", 4, 3, True),
+    ("fake_aug_10_grid", "r_shape", 0, 0, True),
+    ("fake_aug_10_grid", "r_shape", 1, 2, True),
+    ("fake_aug_10_grid", "b_shape", 10, 10, False),
+
+])
+def test_can_place(in_grid_fixture, block_fixture, x, y,  expected_output, request):
+    in_grid = request.getfixturevalue(in_grid_fixture)
+    block = request.getfixturevalue(block_fixture)
+
+    assert can_place(in_grid, block, x, y) == expected_output
