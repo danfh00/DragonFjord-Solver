f5108645c09b8a26bea6690e81c433e8cb56d061
danfh00 <danielfhudson@yahoo.com>
2024-07-11 16:15:23 +0100

    Tests neatened

--- test_solver.py
+++ test_solver.py
@@ -14,29 +14,14 @@ from solver import (
     recursion
 )
 
-jan_1st_grid = [[" ", "2", "3", "4", "5", "6", " "],
-                ["7", "8", "9", "10", "11", "12", " "],
-                [" ", "2", "3", "4", "5", "6", "7"],
-                ["8", "9", "10", "11", "12", "13", "14"],
-                ["15", "16", "17", "18", "19", "20", "21"],
-                ["22", "23", "24", "25", "26", "27", "28"],
-                ["29", "30", "31", " ", " ", " ", " "]]
 
-jun_29th_grid = [["1", "2", "3", "4", "5", " ", " "],
-                 ["7", "8", "9", "10", "11", "12", " "],
-                 ["1", "2", "3", "4", "5", "6", "7"],
-                 ["8", "9", "10", "11", "12", "13", "14"],
-                 ["15", "16", "17", "18", "19", "20", "21"],
-                 ["22", "23", "24", "25", "26", "27", "28"],
-                 [" ", "30", "31", " ", " ", " ", " "]]
-
-
-@pytest.mark.parametrize("test_date, expected_grid", [
-    (date(2023, 1, 1), jan_1st_grid),
-    (date(2010, 6, 29), jun_29th_grid),
+@pytest.mark.parametrize("test_date, expected_grid_fixture", [
+    (date(2023, 1, 1), "fake_jan_1_grid"),
+    (date(2010, 6, 29), "fake_jun_29_grid"),
 ])
 @patch("solver.date")
-def test_make_todays_grid(mock_date, test_date, expected_grid):
+def test_make_todays_grid(mock_date, test_date, expected_grid_fixture, request):
     mock_date.today.return_value = test_date
+    expected_grid = request.getfixturevalue(expected_grid_fixture)
 
     assert make_todays_grid() == expected_grid
